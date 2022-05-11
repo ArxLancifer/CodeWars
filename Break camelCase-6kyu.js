76d4dd91b87751c3fb9e94271e88a4af22aa9958
ArxLancifer <86281760+ArxLancifer@users.noreply.github.com>
2022-05-11 03:23:43 +0300

    Delete Break camelCase-6kyu.js

--- Break camelCase-6kyu.js
+++
@@ -1,6 +0,0 @@
-/*Complete the solution so that the function will break up camel casing, using a space between words. */
-
-function solution(string) {
-  
-    return string.replace(/([A-Z])/g, ' $1')
-}
