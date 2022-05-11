8020124ada8544de5a6be36b2e7f9da1c7fe2f0d
ArxLancifer <86281760+ArxLancifer@users.noreply.github.com>
2022-05-11 03:27:50 +0300

    Delete Numerical Palindrome #1-7kyu.js

--- Numerical Palindrome #1-7kyu.js
+++
@@ -1,9 +0,0 @@
-/*A palindrome is a word, phrase, number, or other sequence of characters which reads the same
- backward as forward. Examples of numerical palindromes are: */
-
-function palindrome(num) { 
-
-    let rev = String(num).split("").reverse().join('')
-    if (typeof num !== 'number' || num<=0) {return "Not valid";}
-    return num === +rev ? true:false;
-  } 
