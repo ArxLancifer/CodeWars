baa7dc806152b6202c1baae6688afb7ff1759bbd
ArxLancifer <a_christoforidis@hotmail.com>
2022-05-13 04:31:42 +0300

    x

--- Highest Scoring Word-6kyu.js
+++ Highest Scoring Word-6kyu.js
@@ -4,14 +4,13 @@ You need to return the highest scoring word as a string.
 If two words score the same, return the word that appears earliest in the original string.
 All letters will be lowercase and all inputs will be valid. */
 
-function high(x){
+function high(x) {
     let myObj = {}
-    for(let key of x.split(' ')){
-      myObj[key] = myObj[key]+1 
+    for (let key of x.split(' ')) {
+        myObj[key] = myObj[key] + 1  //creating object 
     }
-    for(let prop in myObj){
-     myObj[prop] = prop.split("").reduce((acum,char)=>acum+=char.charCodeAt(0)-96,0)
+    for (let prop in myObj) {
+        myObj[prop] = prop.split("").reduce((acum, char) => acum += char.charCodeAt(0) - 96, 0)  //adding values to myObj properties
     }
-   
-    return Object.keys(myObj).reduce((high, word)=> myObj[high] >= myObj[word] ? high:word,0)
-  }
+    return Object.keys(myObj).reduce((high, word) => myObj[high] >= myObj[word] ? high : word, 0)
+}
