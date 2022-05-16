369ba49e53af005b21e05815e8c800d11f0edd5a
ArxLancifer <a_christoforidis@hotmail.com>
2022-05-16 21:34:34 +0300

    CodeWars

---
+++ Simple string characters-7kyu.js
@@ -0,0 +1,25 @@
+/*
+In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+*/
+
+function solve(s) {
+  let upper = 0;
+  let lower = 0;
+  let spec = 0;
+  let num = 0;
+  let x = 1;
+
+  s.split("").forEach((elem) => {
+    console.log(/[A-Z]/.test(elem));
+    if (/[0-9]/.test(elem)) {
+      num++;
+    } else if (/[A-Z]/.test(elem)) {
+      upper++;
+    } else if (/[a-z]/.test(elem)) {
+      lower++;
+    } else {
+      spec++;
+    }
+  });
+  return [upper, lower, num, spec];
+}
