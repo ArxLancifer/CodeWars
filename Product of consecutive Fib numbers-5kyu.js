624703c727e5a9dbd176e954b0206e31fd5c5ad3
ArxLancifer <a_christoforidis@hotmail.com>
2022-06-01 11:55:38 +0300

    CodeWars!

--- Product of consecutive Fib numbers-5kyu.js
+++ Product of consecutive Fib numbers-5kyu.js
@@ -3,13 +3,13 @@
 // such as
 // F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
 
-function productFib(prod){
-    var fir = 0;
-    var sec = 1;
-   while(prod>fir*sec){
-     fir =fir+sec;
-     sec =fir-sec;
-   }
-    
-    return [sec, fir, fir*sec === prod];
+function productFib(prod) {
+  var fir = 0;
+  var sec = 1;
+  while (prod > fir * sec) {
+    fir = fir + sec;
+    sec = fir - sec;
   }
+
+  return [sec, fir, fir * sec === prod];
+}
