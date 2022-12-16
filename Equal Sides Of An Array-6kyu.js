9f8c65631e562b26496fe414659029cdd01d5773
ArxLancifer <a_christoforidis@hotmail.com>
2022-12-16 17:34:44 +0200

    CodeWars!

---
+++ Equal Sides Of An Array-6kyu.js
@@ -0,0 +1,21 @@
+// You are going to be given an array of integers.
+//  Your job is to take that array and find an index N where the
+//  sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+
+function findEvenIndex(arr)
+{
+  let result;
+  let leftSide;
+  let rightSide;
+  for(let i = 0; i<=arr.length; i++){
+    leftSide = arr.slice(0, i).reduce((acc, num)=>{return acc+=num},0);
+    
+    rightSide = arr.slice(i+1, arr.length).reduce((acc, num)=>{return acc+=num},0);
+    console.log(leftSide, rightSide)
+    if(leftSide == rightSide) {
+      return i;
+    };
+  }
+  if(!result) return -1; 
+}
